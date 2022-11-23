f149530d67db2e72183a9fc0bf64b1c9aaa9f3a1
purreB <jakob.bergstrom@live.se>
2022-11-23 23:32:38 +0100

    added 'main' controller

--- file_api.Api/src/main.rs
+++ file_api.Api/src/main.rs
@@ -1,8 +1,29 @@
 //TODO: Add Controllers with actix web, make seperate [file]Controller for each filetype
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
 extern crate file_api_core;
 extern crate file_api_persistence;
 
-fn main() {
-    println!("Hello, world!");
+#[post("/upload-file")]
+async fn upload_file() -> impl Responder {
+    HttpResponse::Ok().body("Upload file route here!")
+}
+
+#[get("/download-file")]
+async fn download_file() -> impl Responder {
+    HttpResponse::Ok().body("Download file route here!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new().service(
+            web::scope("/api")
+                .service(upload_file)
+                .service(download_file),
+        )
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
