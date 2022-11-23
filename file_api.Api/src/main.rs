06d6d560c0e99082e2111c40b251221799e58fbe
purreB <jakob.bergstrom@live.se>
2022-11-23 22:56:55 +0100

    Renamed modules

--- file_api.Api/src/main.rs
+++ file_api.Api/src/main.rs
@@ -1,6 +1,7 @@
 //TODO: Add Controllers with actix web, make seperate [file]Controller for each filetype
 
-extern crate file_api_Core;
+extern crate file_api_core;
+extern crate file_api_persistence;
 
 fn main() {
     println!("Hello, world!");
