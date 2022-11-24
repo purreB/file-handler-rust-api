b40334027e408dfb30909cdde08123b64ea6250e
purreB <jakob.bergstrom@live.se>
2022-11-24 21:37:37 +0100

    Created a pdf model, hopefully auto generates a uuid and creates an empty byte array to later be written into

--- file_api.Core/src/module_domain/models.rs
+++ file_api.Core/src/module_domain/models.rs
@@ -1,6 +1,10 @@
-extern crate guid_create;
-use guid_create::GUID;
+use byte_array::ByteArray;
+use uuid::Uuid;
 
 pub struct pdf {
-    pub guid: GUID::rand(),
+    pub id: Uuid::new_v4(),
+    pub name: String,
+    pub file_size: usize,
+    pub content: ByteArray::new(),
+    pub cheksum: String,
 }
